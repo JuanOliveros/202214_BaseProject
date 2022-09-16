a66cafa29a8cbaf552b1d5a32ec6ff2b5bf500ca
JuanOliveros <j.oliverosv@uniandes.edu.co>
2022-09-16 15:41:38 -0500

    Todo lo que es persistencia
    Todo lo que es persistencia

--- src/aerolinea/aerolinea.entity.ts
+++ src/aerolinea/aerolinea.entity.ts
@@ -1,1 +1,30 @@
-export class AerolineaEntity {}
+import { AeropuertoEntity } from '../aeropuerto/aeropuerto.entity';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class AerolineaEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  nombre: string;
+
+  @Column()
+  descripcion: string;
+
+  @Column()
+  fecha_fundacion: string;
+
+  @Column()
+  pagina_web: string;
+
+  @ManyToMany(() => AeropuertoEntity, (aeropuerto) => aeropuerto.aerolineas)
+  @JoinTable()
+  aeropuertos: AeropuertoEntity[];
+}
