f75e992b10c9381f1def4e01a697d5624d4099f2
JuanOliveros <j.oliverosv@uniandes.edu.co>
2022-09-16 15:10:39 -0500

    Crear clases del recurso
    Crear clases del recurso (Aerolinea entity e Aeropuesto Entity)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MuseumModule } from './museum/museum.module';
+import { AerolineaModule } from './aerolinea/aerolinea.module';
+import { AeropuertoModule } from './aeropuerto/aeropuerto.module';
 
 @Module({
-  imports: [],
+  imports: [MuseumModule, AerolineaModule, AeropuertoModule],
   controllers: [AppController],
   providers: [AppService],
 })
