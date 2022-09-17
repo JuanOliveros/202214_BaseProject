eb7bb5a3a8187ffd7076fd9b4960705365f87084
JuanOliveros <j.oliverosv@uniandes.edu.co>
2022-09-16 19:06:34 -0500

    Pruebas logicas aerolineas y aeropuertos
    Se realizan las pruebas logicas aerolineas y aeropuertos

--- src/aeropuerto/aeropuerto.service.spec.ts
+++ src/aeropuerto/aeropuerto.service.spec.ts
@@ -1,18 +1,135 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AeropuertoService } from './aeropuerto.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { AeropuertoEntity } from './Aeropuerto.entity';
+import { AeropuertoService } from './Aeropuerto.service';
+import { faker } from '@faker-js/faker';
 
-describe('AeropuertoService', () => {
+describe('AeropuertoaService', () => {
   let service: AeropuertoService;
+  let repository: Repository<AeropuertoEntity>;
+  let aeropuertoList: AeropuertoEntity[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [AeropuertoService],
     }).compile();
 
     service = module.get<AeropuertoService>(AeropuertoService);
+    repository = module.get<Repository<AeropuertoEntity>>(
+      getRepositoryToken(AeropuertoEntity),
+    );
+    await seedDatabase();
   });
+
+  const seedDatabase = async () => {
+    repository.clear();
+    aeropuertoList = [];
+    for (let i = 0; i < 5; i++) {
+      const aeropuerto: AeropuertoEntity = await repository.save({
+        nombre: faker.company.name(),
+        codigo: faker.phone.imei(),
+        pais: faker.address.county(),
+        ciudad: faker.address.city(),
+      });
+      aeropuertoList.push(aeropuerto);
+    }
+  };
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findAll should return all aeropuertos', async () => {
+    const aeropuertos: AeropuertoEntity[] = await service.findAll();
+    expect(aeropuertos).not.toBeNull();
+    expect(aeropuertos).toHaveLength(aeropuertoList.length);
+  });
+
+  it('findOne should return a aeropuerto by id', async () => {
+    const storedAeropuerto: AeropuertoEntity = aeropuertoList[0];
+    const aeropuerto: AeropuertoEntity = await service.findOne(
+      storedAeropuerto.id,
+    );
+    expect(aeropuerto).not.toBeNull();
+    expect(aeropuerto.nombre).toEqual(storedAeropuerto.nombre);
+    expect(aeropuerto.codigo).toEqual(storedAeropuerto.codigo);
+    expect(aeropuerto.pais).toEqual(storedAeropuerto.pais);
+    expect(aeropuerto.ciudad).toEqual(storedAeropuerto.ciudad);
+  });
+
+  it('findOne should throw an exception for an invalid aeropuerto', async () => {
+    await expect(() => service.findOne('0')).rejects.toHaveProperty(
+      'message',
+      'El aeropuerto con el id dado no fue encontrado',
+    );
+  });
+
+  it('create should return a new aeropuerto', async () => {
+    const aeropuerto: AeropuertoEntity = {
+      id: '',
+      nombre: faker.company.name(),
+      codigo: faker.phone.imei(),
+      pais: faker.address.county(),
+      ciudad: faker.address.city(),
+      aerolineas: [],
+    };
+    const newAeropuerto: AeropuertoEntity = await service.create(aeropuerto);
+    expect(newAeropuerto).not.toBeNull();
+    const storedAeropuerto: AeropuertoEntity = await repository.findOne({
+      where: { id: newAeropuerto.id },
+    });
+    expect(aeropuerto).not.toBeNull();
+    expect(aeropuerto.nombre).toEqual(storedAeropuerto.nombre);
+    expect(aeropuerto.codigo).toEqual(storedAeropuerto.codigo);
+    expect(aeropuerto.pais).toEqual(storedAeropuerto.pais);
+    expect(aeropuerto.ciudad).toEqual(storedAeropuerto.ciudad);
+  });
+
+  it('update should modify a aeropuerto', async () => {
+    const aeropuerto: AeropuertoEntity = aeropuertoList[0];
+    aeropuerto.nombre = 'New aeropuerto';
+    const updatedAeropuerto: AeropuertoEntity = await service.update(
+      aeropuerto.id,
+      aeropuerto,
+    );
+    expect(updatedAeropuerto).not.toBeNull();
+
+    const storedAeropuerto: AeropuertoEntity = await repository.findOne({
+      where: { id: aeropuerto.id },
+    });
+    expect(storedAeropuerto).not.toBeNull();
+    expect(storedAeropuerto.nombre).toEqual(storedAeropuerto.nombre);
+  });
+
+  it('update should throw an exception for an invalid aeropuerto', async () => {
+    let aeropuerto: AeropuertoEntity = aeropuertoList[0];
+    aeropuerto = {
+      ...aeropuerto,
+      nombre: 'New aeropuerto',
+    };
+    await expect(() => service.update('0', aeropuerto)).rejects.toHaveProperty(
+      'message',
+      'El aeropuerto con el id dado no fue encontrado',
+    );
+  });
+
+  it('delete should remove a aeropuerto', async () => {
+    const aeropuerto: AeropuertoEntity = aeropuertoList[0];
+    await service.delete(aeropuerto.id);
+    const deletedAeropuerto: AeropuertoEntity = await repository.findOne({
+      where: { id: aeropuerto.id },
+    });
+    expect(deletedAeropuerto).toBeNull();
+  });
+  it('delete should throw an exception for an invalid aeropuerto', async () => {
+    const aeropuerto: AeropuertoEntity = aeropuertoList[0];
+    await service.delete(aeropuerto.id);
+    await expect(() => service.delete('0')).rejects.toHaveProperty(
+      'message',
+      'El aeropuerto con el id dado no fue encontrado',
+    );
   });
 });
